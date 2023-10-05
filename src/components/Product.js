1de860c328652c0ef284f5ba4a6c622202d8a153
Bushra Shahzad <bushrashahzad2001@gmail.com>
2023-10-05 15:47:51 +0500

    first commit

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,9 +6,14 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import {useState, useEffect} from "react";
 import axios from "axios";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import Container from 'react-bootstrap/Container';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
  function Product() {
   const [products,setProduct] = useState([]);
 
@@ -25,10 +30,18 @@ console.log(products)
 useEffect(() => {
  getData();
 }, [])
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
   const content = products && products.map(product => {
-    return (
-      <Col>
+    return ( 
+      <Box sx={{ flexGrow: 1 }}>
+      <Grid>
       <Card sx={{ maxWidth: 200 }}>
       <CardActionArea>
         <CardMedia
@@ -50,17 +63,13 @@ useEffect(() => {
         </CardContent>
       </CardActionArea>
     </Card>
-    </Col>
-  
+   </Grid>
+   </Box>
   )})
   
   return (
     <>
-   <Container>
-      <Row>
-    {content}  
-    </Row>
-    </Container>
+    {content}
     </>
   );
 }
